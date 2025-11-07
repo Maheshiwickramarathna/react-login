1fe491ee78059b97a6958147dbd6e34055ed8ad5
Maheshi Wickramarathna <maheshiwickramarathna2001@gmail.com>
2025-11-07 20:19:55 +0530

    create forget pass interface

--- src/Pages/Forrget.js
+++ src/Pages/Forrget.js
@@ -1,23 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../Style/Forget.css'
+import { Link } from 'react-router-dom'
 export default function Forrget() {
+
+  
   return (
+    
     <div className='forget'>
        
-        <div className='center'>
-            <center>
+        
+            <div className='con'>
                 <form id="forget-form" method='POST'>
-                   <h1>Reset Your Password</h1>
-                    <input type="passwor" name="pwd1" placeholder="Enter new password"/>
-                   
-                    <input type="password" name="pwd" placeholder="Re-enter"/>
+                   <h3>Forget Password</h3>
+                   <p>Enter your Email and we'll send you a link to reset your password</p>
                     
-                    <button>Reset</button>
+                   
+                    <input type="email" name="em" placeholder="Enter email"/>
+                    
+                    <button>Submit</button>
+                    <Link to= '/'> Back to Login</Link>
+                 
                    
                     
                 </form>
-            </center>
-        </div>
+           </div>
+       
     </div>
   )
 }
